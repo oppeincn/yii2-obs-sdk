2aa1187a93b1462a4294426f7128891a2a9e6ebf
werelean <yzxy123506@outlook.com>
2021-03-22 17:25:40 +0800

    更新说明“

--- obssdk.php
+++ obssdk.php
@@ -5,7 +5,7 @@
  * @license http://opensource.org/licenses/mit-license.php The MIT License (MIT)
  * @package yii2-aws-sdk
  */
-namespace oppein\obssdk;
+namespace oppeincn\obssdk;
 use yii\base\Component;
 use Aws;
 
